fb090ed9a9d4b0262d7d49d1233b62936380ed36
asge4900 <48907330+asge4900@users.noreply.github.com>
2020-03-16 10:32:46 +0100

    Add project files.

---
+++ HTKTennisclub.Repository/Routes/ApiControllers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTKTennisclub.Repository.Routes
+{
+    public static class ApiControllers
+    {
+       
+    }
+}
---
+++ HTKTennisclub.Repository/Routes/Url.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTKTennisclub.Repository.Routes
+{
+    public static class Url
+    {
+        /// <summary>
+        /// Base URL for the app's API service      
+        /// </summary>
+        public const string ApiUrl = @"http://localhost:58861/api/";
+    }
+}
